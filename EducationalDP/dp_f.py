6b07254052f49c93046673c8223c2be8e06997b4
nao1412 <naoya_greeeen_0720@icloud.com>
2021-06-18 20:27:44 +0900

    desk

--- EducationalDP/dp_f.py
+++ EducationalDP/dp_f.py
@@ -13,5 +13,26 @@ def main():
   s = S()
   t = S()
   
+  ls = len(s)
+  lt = len(t)
+
+  dp = [[0]*(lt+1) for _ in range(ls+1)]
+  # print(dp)
+  for i in range(ls):
+    for j in range(lt):
+      if s[i] == t[j]: dp[i+1][j+1] = dp[i][j] + 1
+      else: dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+
+  ans = ''
+  i = ls
+  j = lt
+  while i != 0 and j != 0:
+    if dp[i][j] == dp[i][j-1]: j -= 1
+    elif dp[i][j] == dp[i-1][j]: i -= 1
+    else:
+      i -= 1; j -= 1
+      ans += s[i]
+      
+  print(ans[::-1])
 if __name__ == '__main__':
   main()
